a21d0a87bbc1656a0e787d494b69ad8e99ff79d6
Timothy Bennett <bennett.timothy.j@gmail.com>
2019-10-30 22:02:47 -0500

    new code

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,42 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+// import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = ({ siteTitle }) => {
+    const [open, setOpen] = React.useState(false);
+
+    const toggleMenu = () => {
+        setOpen(!open);
+    }
+
+    return (
+        <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="https://bulma.io">
+                    <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+                </a>
+
+                <a role="button" className="navbar-burger burger" onClick={toggleMenu} aria-label="menu" aria-expanded="false">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div className={`${ open ? 'is-active ' : '' }navbar-menu`}>
+                <div className="navbar-end">
+                    <a className="navbar-item">
+                        Home
+                    </a>
+
+                    <a className="navbar-item">
+                        Documentation
+                    </a>
+                </div>
+            </div>
+        </nav>
+    )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,42 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <section class="hero is-dark is-medium">
+        <div class="hero-body">
+            <div class="container">
+            <h1 class="title">
+                Medium title
+            </h1>
+            <h2 class="subtitle">
+                Medium subtitle
+            </h2>
+            </div>
+        </div>
+    </section>
+    <div className="container">
+        <div className="section is-large"></div>
+        <div className="section is-large"></div>
+        <div className="section is-large"></div>
+        <div className="section is-large"></div>
+        <div className="section is-large"></div>
+        <div className="section is-large"></div>
+        <div className="section is-large"></div>
+        <div className="section is-large"></div>
+        <div className="section is-large"></div>
+        <div className="section is-large"></div>
+        <div className="section is-large"></div>
+        <div className="section is-large"></div>
+        <div className="section is-large"></div>
+        <div className="section is-large"></div>
+        <div className="section is-large"></div>
+        <div className="section is-large"></div>
+        <div className="section is-large"></div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
